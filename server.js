78e12767a9b76c6cd14df648414efb21fa29cdf6
Dylan Thompson <dylant7192@gmail.com>
2026-06-13 18:50:34 +0100

    fix: fixed the fix which fixed a fix to a fix

--- server.js
+++ server.js
@@ -4,15 +4,11 @@ const PORT = process.env.PORT || 8888;
 
 const express = require('express');
 const app = express();
-app.use(express.static('client'));
+app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.json());
 
 // API Endpoints
 
-
-app.get('/', (_, res) => {
-  res.sendFile(path.join(__dirname, 'client/index.html'));
-});
 
 
 // END
